6b51ba5a6b04cb83ed08ea8b01d9aac7aeb355a2
Gustavo Keller <gustavo.lealtec@gmail.com>
2022-11-23 21:43:08 -0300

    update tarefas

--- todas_tarefas.php
+++ todas_tarefas.php
@@ -2,6 +2,10 @@
 
 	$acao = 'recuperar';
 	require 'tarefa_controller.php';
+
+	// echo '<pre>';
+	// print_r($tarefas);
+	// echo '</pre>';
 	
 ?>
 
@@ -42,24 +46,19 @@
 							<div class="col">
 								<h4>Todas tarefas</h4>
 								<hr />
+								
+								<?php foreach($tarefas as $indice => $tarefa) { ?>
 
-								<div class="row mb-3 d-flex align-items-center tarefa">
-									<div class="col-sm-9">Lavar o carro (status)</div>
-									<div class="col-sm-3 mt-2 d-flex justify-content-between">
-										<i class="fas fa-trash-alt fa-lg text-danger"></i>
-										<i class="fas fa-edit fa-lg text-info"></i>
-										<i class="fas fa-check-square fa-lg text-success"></i>
+									<div class="row mb-3 d-flex align-items-center tarefa">
+										<div class="col-sm-9"><?= $tarefa->tarefa ?> (<?= $tarefa->status ?>)</div>
+										<div class="col-sm-3 mt-2 d-flex justify-content-between">
+											<i class="fas fa-trash-alt fa-lg text-danger"></i>
+											<i class="fas fa-edit fa-lg text-info"></i>
+											<i class="fas fa-check-square fa-lg text-success"></i>
+										</div>
 									</div>
-								</div>
 
-								<div class="row mb-3 d-flex align-items-center tarefa">
-									<div class="col-sm-9">Passear com o cachorro (status)</div>
-									<div class="col-sm-3 mt-2 d-flex justify-content-between">
-										<i class="fas fa-trash-alt fa-lg text-danger"></i>
-										<i class="fas fa-edit fa-lg text-info"></i>
-										<i class="fas fa-check-square fa-lg text-success"></i>
-									</div>
-								</div>
+								<?php } ?>
 								
 							</div>
 						</div>
